060ae761134757de84e43e416fe4ca4335103a5d
Alasdair <alex-603@hotmail.com>
2013-12-23 00:02:44 +0000

    updating arduino code

--- CloseEnc3_v1/CloseEnc3_v1.ino
+++ CloseEnc3_v1/CloseEnc3_v1.ino
@@ -24,16 +24,22 @@
      if (usbnumber > 0) {
          switch(usbnumber){
                  case 49:  setColor(255, 0, 0); //red - G
+                           tone(8,392);
                    break;
                  case 50:  setColor(255, 140, 0);//Orange - A
+                           tone(8,440);
                    break;
                  case 51:  setColor(80, 0, 80); //Purple - F
+                           tone(8,349);
                    break;
                  case 52:  setColor(255, 255, 0); //Yellow - F an octave lower
+                           tone(8,175);
                    break;
                  case 53:  setColor(250, 250, 250); // White - C
+                           tone(8,262);
                    break;
                  case 54:  setColor(0, 0, 0); // off
+                           noTone(8);
                    break;
                }
     }
